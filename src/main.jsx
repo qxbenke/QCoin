fc8a051774874ca42b4892fb3da917ecd4d3f881
qxbenke <dengwenxianglove@gmail.com>
2026-05-27 22:54:07 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl="https://q-coin-green.vercel.app/tonconnect-manifest.json">
+      <App />
+    </TonConnectUIProvider>
   </React.StrictMode>
 )
